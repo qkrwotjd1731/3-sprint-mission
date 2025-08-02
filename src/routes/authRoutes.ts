689641dd088eaa66cfaf66d0c5ed2aeaf8c86de7
qkrwotjd1731 <58104546+qkrwotjd1731@users.noreply.github.com>
2025-08-03 04:44:52 +0900

    tsx 설치 및 오류 수정

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,13 +2,13 @@ import { Router } from 'express';
 import { asyncHandler } from '../lib/asyncHandler';
 import * as authController from '../controllers/authController';
 import { validateCreateUser, validateLogin } from '../validators/validateAuth';
-import { verifyRefreshToken } from '../middlewares/auth';
+import { verifyAccessToken, verifyRefreshToken } from '../middlewares/auth';
 
 const authRouter = Router();
 
 authRouter.post('/signup', validateCreateUser, asyncHandler(authController.createUser));
 authRouter.post('/login', validateLogin, asyncHandler(authController.login));
-authRouter.post('/logout', asyncHandler(authController.logout));
+authRouter.post('/logout', verifyAccessToken, asyncHandler(authController.logout));
 authRouter.post('/refresh', verifyRefreshToken, asyncHandler(authController.refreshToken));
 
 export default authRouter; 
